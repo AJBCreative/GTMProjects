683d5fd41f4343d8accffe8bd586ea3e4f25d680
AJBCreative <49920399+AJBCreative@users.noreply.github.com>
2025-05-30 10:21:43 -0400

    Update layout.js

--- nextjs-app/app/layout.js
+++ nextjs-app/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css';
-import Script from 'next/script'; // Import the Script component
+// import Script from 'next/script'; // <--- REMOVE this line if you added it for next/script
 
 export const metadata = {
   title: 'GTM Testing Ground - Script on Load',
@@ -10,14 +10,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager (head script) */}
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K4RRXNNR'});`}
-        </Script>
+        {/* Google Tag Manager (head script) - USING dangerouslySetInnerHTML for reliability */}
+        <script
+          id="google-tag-manager-script" // Give it an ID if you want
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K4RRXNNR}');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}

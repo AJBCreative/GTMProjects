8c840c3e82f53bf2e15e696729c143827ab8dafb
AJBCreative <49920399+AJBCreative@users.noreply.github.com>
2025-05-30 13:20:57 -0400

    Update next.config.mjs

--- nextjs-app/next.config.mjs
+++ nextjs-app/next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   output: 'export', // Ensures a static HTML export
 
   // Set the base path for all your assets and links to your repository name
-  basePath: '/GTMProjects', // Matches your GitHub repository name
+  basePath: '/gtmprojects', // Matches your GitHub repository name
 
-  assetPrefix: '/GTMProjects/', // Matches your GitHub repository name, with a trailing slash
+  assetPrefix: '/gtmprojects/', // Matches your GitHub repository name, with a trailing slash
 
   images: {
     unoptimized: true, // Required for static export, as Next.js image optimization needs a server
